16d1489e34e81c60bf42c24b91d050554a9f9d73
Razz700 <160166041+Razz700@users.noreply.github.com>
2024-06-25 12:22:22 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@
 //   )
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+//import './index.css';
+import App from './components/App';
 //import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
